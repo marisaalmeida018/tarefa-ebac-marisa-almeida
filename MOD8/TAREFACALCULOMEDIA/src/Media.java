3920b7ed6fcc657eb2a7097fc62b59139ca953ec
Marisa Almeida <marisaalmeida018@gmail.com>
2025-11-07 11:32:45 -0300

    tarefa 8 calculo media

---
+++ MOD8/TAREFACALCULOMEDIA/src/Media.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Criar a média de 4 notas
+ */
+
+public class Media {
+    public static void main(String[] args) {
+      int nota1 = 5;
+      int nota2 = 7;
+      int nota3 = 8;
+      int nota4 = 4;
+      int media = (nota1 + nota2 + nota3 + nota4) / 4;
+      System.out.println(media);
+
+
+    }
+}
