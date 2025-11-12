938ca3d9f06236a9172f275a1c2e5f413feb7658
Marisa Almeida <marisaalmeida018@gmail.com>
2025-11-12 19:19:08 -0300

    Exercício módulo 9

---
+++ MOD9/ExercicioMod9/src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Ler um valor numérico na variável primitiva, converter para wrapper e imprimir novamente
+ */
+
+public class Main {
+    public static void main(String[] args) {
+      long cpf = 12345678L;
+      System.out.println(cpf);
+      Long cpf1 = cpf;
+      System.out.println(cpf1);
+
+
+    }
+
+}
