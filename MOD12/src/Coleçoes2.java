76522e8c7bdf92bdfda2d39067040b1735479ed5
Marisa Almeida <marisaalmeida018@gmail.com>
2026-02-20 13:09:07 -0300

    Coleções 2

---
+++ MOD12/src/Coleçoes2.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Coleçoes2 {
+    public static void main(String[] args) {
+
+        String nomes = ("Amanda - F, Ivan - M, Yuri - M, Vitória - F, Juliana - F");
+        String[] pessoa = nomes.split(", ");
+
+        Map<String, List<String>> grupos = new HashMap<>();
+        for (String parte : pessoa) {
+            parte = parte.trim();
+            String[] nomeGenero = parte.split("-");
+            String nome =  nomeGenero[0];
+            String genero = nomeGenero[1];
+            grupos.computeIfAbsent(genero, k -> new ArrayList<>()).add(nome);
+
+        }
+        for (Map.Entry<String, List<String>> entry : grupos.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+    }
+
+
+}
+
